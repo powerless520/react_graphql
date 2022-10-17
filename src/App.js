e278cc257ff6d4e110cab40acecbb369bffc575c
powerless <asphalt1018@gmail.com>
2022-10-17 17:49:32 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import {useQuery, gql} from "@apollo/client";
 
 function App() {
-    return (
-        <div>
-            <h2>My first Apollo app 🚀</h2>
-            <br/>
-            <DisplayLocations />
-        </div>
-    );
-}
-
-const GET_LOCATIONS = gql`
-query GetLaunches {
-    launches {
-        cursor
-        hasMore
-        launches {
-            id
-            site
-            mission {
-                name
-                missionPatch
-            }
-            isBooked
-            rocket {
-                id
-                name
-                type
-            }
-        }
-    }
-}
-`;
-
-function DisplayLocations() {
-    const {loading, error, data} = useQuery(GET_LOCATIONS);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-    return <div>{JSON.stringify(data)}</div>
-
-//  return data.locations.map(({ id, name, description, photo }) => (
-//      <div key={id}>
-//        <h3>{name}</h3>
-//        <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-//        <br />
-//        <b>About this location:</b>
-//        <p>{description}</p>
-//        <br />
-//      </div>
-//  ));
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,44 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
-
-const client = new ApolloClient({
-    uri: 'https://apollo-fullstack-tutorial.herokuapp.com/graphql',
-    cache: new InMemoryCache(),
-})
-
-client.query({
-    query: gql`
-  query GetLocations {
-    launches {
-        cursor
-        hasMore
-        launches {
-            id
-            site
-            mission {
-                name
-                missionPatch
-            }
-            isBooked
-            rocket {
-                id
-                name
-                type
-            }
-        }
-    }
-  }
-    `,
-})
-    .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ApolloProvider client={client}>
-        <App/>
-    </ApolloProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
