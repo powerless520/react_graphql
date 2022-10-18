44f3aeba9284eb38f8808f8480fd9ffaeec1d23f
powerless <asphalt1018@gmail.com>
2022-10-18 18:30:15 +0800

    react study

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const hText = <h1>Hello React</h1>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = 'John Perez';
+const element = <h1>Goods {name}</h1>
+
+const
+root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
         <h1>Hello World!</h1>
-        <App />
+        <div>{element}</div>
+        <App/>
     </div>
-
 );
 
 reportWebVitals()
